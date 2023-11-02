d8f6681916467ae7b118e910f467120e2115d261
Anastasya <gbshell2022@gmail.com>
2023-11-02 13:11:09 +0300

    решение 1 задачи к 7 семинару

---
+++ sem7-z1/Program.cs
@@ -0,0 +1,34 @@
+﻿Random random = new Random();
+
+  public static double[, ] CreateRandomMatrix(int m, int n, int minLimitRandom, int maxLimitRandom) {
+    double[, ] matrix = new double[m, n];
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        matrix[i, j] = random.Next(minLimitRandom, maxLimitRandom + 1) + random.NextDouble();
+      }
+    }
+
+    return matrix;
+  }
+
+  public static void PrintArray(double[, ] matrix) {
+    for (int i = 0; i < matrix.GetLength(0); i++) {
+      for (int j = 0; j < matrix.GetLength(1); j++) {
+        Console.Write($"{matrix[i, j]:f2}\t");
+      }
+      Console.WriteLine();
+    }
+  }
+
+  public static void Main(string[] args) {
+    int m, n, minLimitRandom, maxLimitRandom;
+
+    if (args.Length >= 4) {
+      m = int.Parse(args[0]);
+      n = int.Parse(args[1]);
+      minLimitRandom = int.Parse(args[2]);
+      maxLimitRandom = int.Parse(args[3]);
+
+      double[,] array = CreateRandomMatrix(m, n, minLimitRandom, maxLimitRandom);
+
